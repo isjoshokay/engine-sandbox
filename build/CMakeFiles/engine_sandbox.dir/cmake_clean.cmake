31a1946cee9be39befe6dd75a7ef313ce23027e3
jpcoder12 <jpcoder12@gmail.com>
2025-12-03 11:42:10 -0700

    Add CMake build system output files
    Initial commit of CMake-generated build files, including cache, compiler configuration, build scripts, and output artifacts for the engine_sandbox project. These files are produced by running CMake to configure the build environment.

---
+++ build/CMakeFiles/engine_sandbox.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/engine_sandbox.dir/src/main.cpp.o"
+  "CMakeFiles/engine_sandbox.dir/src/main.cpp.o.d"
+  "engine_sandbox"
+  "engine_sandbox.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/engine_sandbox.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
