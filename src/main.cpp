d9b7b5b0a5b1ba2b71265f4eecc868d2875250a0
jpcoder12 <jpcoder12@gmail.com>
2025-12-05 03:01:49 -0700

    SDL Renderer intialization/background
    adjusted comments and wording on notes, exploring SDL renderer.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#include <SDL2/SDL.h> // you must have your system's path to sdl2 in cmake for this line to work
+#include <SDL2/SDL.h> // you must have your system's path to sdl2 in makefile for this line to work
 #include <iostream>
 
 int main() {
@@ -17,7 +17,7 @@ int main() {
         SDL_WINDOWPOS_CENTERED, // where, but horizontally
         SDL_WINDOWPOS_CENTERED, // where, but vertically
         800, 600, // window sizing in px
-        SDL_WINDOW_SHOWN // flags
+        SDL_WINDOW_SHOWN // bitwise flag
     );
     if (!window) { // err handling for window obj
         std::cerr << "SDL_CreateWindow failed: " << SDL_GetError() << "\n";
@@ -25,20 +25,46 @@ int main() {
         return 1;
     }
 
+    // Create a renderer (added 12/5/25)
+    SDL_Renderer* renderer = SDL_CreateRenderer(
+        window, // (window that was created)
+        -1, // "index of the rendering driver???" 
+        SDL_RENDERER_ACCELERATED // flag for hardware acceleration (if possible)
+    );
+    if (!renderer){ // rendering without a renderer is a harmful semi-alliterative phrase
+        std::cerr << "SDL_CreateRenderer failed: " << SDL_GetError() << std::endl;
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    } else {
+        std::cout << "Renderering the renderation...";
+    }
+
     bool running = true;
     SDL_Event event;
 
     while (running) { // main loop
         while (SDL_PollEvent(&event)) { // 
-            if (event.type == SDL_QUIT) { // (user clicked x)
+            if (event.type == SDL_QUIT) { // (user clicked x or force quit)
                 running = false;
             }
         }
-        // rendering will go here.
+        // -- RENDERING STARTS HERE -- 
+        // set color (RGBA)
+        SDL_SetRenderDrawColor(renderer, 150, 30, 100, 255); 
+        SDL_RenderClear(renderer); 
+
+        // draw calls will go here
+
+        SDL_RenderPresent(renderer); // swap buffers: actually shows the frame
+
         SDL_Delay(16); // ~60fps (16 ms -> 1/60)...every iteration of the parent loop = 1 frame
     }
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+
+    // CLEAN UP IN REVERSE ORDER OF CREATION
+    SDL_DestroyRenderer(renderer); // this
+    SDL_DestroyWindow(window); // depends on this
+    SDL_Quit(); // which depends on this
     std::cout << "User exited the program.\n";
     return 0;
 
